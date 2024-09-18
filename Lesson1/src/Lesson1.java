463c0b34586ca330d54928a107575f5124494919
rimagrigoryan12 <grigoryanrimaa84@gmail.com>
2024-09-18 19:27:55 +0400

    My firts git commit

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,54 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Barev");
+        System.out.println("Lesson1");
+
+        byte x = 127;
+        System.out.println(x);
+
+        short y = 4542;
+        System.out.println(y);
+
+        int a = 454654;
+        System.out.println(a);
+
+        long b = 4465L;
+        System.out.println(b);
+
+        //kamayakanamoxjtivjavayuminta
+
+        //kamayakanracional`amoxjtivjavayumdoubla
+        float aa = 455.4F;
+        double bb = 45.4;
+
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+
+        char t = '$';
+        System.out.println(t);
+        char tt = '\u003F';
+        System.out.println(tt);//esamoxj@primitivtiperenugrvumenpoqratarerov,String@parztipchiugrvumemecatarov
+
+        //Stringtype-idepqumgrumenqkamayakantext,kamayakanerkarutyan
+        String s = "barvLesson1";
+        System.out.println(s);
+        //+nshan@Stringihamar
+        String firstname = "Hayk";
+        String lastname = "Avdalyan";
+        System.out.println(firstname + lastname);//bayckaprabelixndir
+        System.out.println(firstname + "" + lastname);//kam,vork-ickaxvacarjeq@poxvi
+
+        int k = 96;
+        System.out.println("k=" + 94);//esdepqumk-iarjeq@chipoxvi,qanivormenqdzerqovenqarjeqtvel,ustivorpisik-npoxvigrumenq
+        System.out.println("k=" + k);
+
+
+    }
+
+}
+
+
